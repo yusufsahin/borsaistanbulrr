a6fb37219989e661873efa061d9281aeef77e728
Yusuf Sahin <yusuf@yusufsahin.net>
2024-12-23 14:56:27 +0300

    first commit

---
+++ helloreact/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App=()=>{
+
+    return(
+        <div>
+            <h1>React Manuel Kurulum</h1>
+            <p>Hello React</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ helloreact/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Çıkış klasörünü temizler
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'public'), // Public klasörünü servis eder
+    port: 3000,
+    hot: true,
+    historyApiFallback: true, // SPA routing için gerekli
+  },
+};
